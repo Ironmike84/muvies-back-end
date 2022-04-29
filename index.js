da3281dcd2eea093df64f08e2096a1c7b658b363
ironmike84 <michaelgross.tn@gmail.com>
2022-04-29 17:00:50 -0400

    killerfours

--- index.js
+++ index.js
@@ -230,7 +230,7 @@ app.put('/Favorites/:UserName/delete/:_id',passport.authenticate('jwt', { sessio
       if (!Favs) {
         res.status(400).send('ID: ' + req.params._id + ' was not found!!');
       } else {
-        Favs.deleteOne({FavoriteMovies: [{ObjectId: req.params._id}]});
+        this.Favs.deleteOne({FavoriteMovies: [{ObjectId: req.params._id}]});
         res.status(200).send('ID: ' + req.params._id + ' was deleted!');
       }
     })
