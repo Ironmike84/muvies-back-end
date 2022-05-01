6c3b709e2b18c676a0a5d056a86ad7c3776b53a3
ironmike84 <michaelgross.tn@gmail.com>
2022-05-01 17:52:57 -0400

    updatedtodaysssssss

--- index.js
+++ index.js
@@ -241,12 +241,12 @@ app.post('/Favorites/:UserName',passport.authenticate('jwt', { session: false })
 
 app.put('/Favorites/:UserName/delete/:_id',passport.authenticate('jwt', { session: false }), (req, res) => {
   users.findOneAndUpdate({ UserName: req.params.UserName }, { $pull: { FavoriteMovies: [{ObjectID: req.params._id }] } })
-    .then((users) => {
+    .then((user) => {
         
-      if (!users) {
+      if (!user) {
         res.status(400).send('ID: ' + req.params._id + ' was not found!!');
       } else {
-        users.findOneAndUpdate({ UserName: req.params.UserName }, { $pull: { FavoriteMovies: [{ObjectID: req.params._id }] } })
+        user.updateOne({ UserName: req.params.UserName }, { $pull: { FavoriteMovies: [{ObjectID: req.params._id }] } })
         res.status(200).send('ID: ' + req.params._id + ' was deleted!');
       }
     })
