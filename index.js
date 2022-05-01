1f42f2b34a3f2cc1fa89240852dbd1815ec62cc5
ironmike84 <michaelgross.tn@gmail.com>
2022-05-01 14:54:36 -0400

    updatedssts

--- index.js
+++ index.js
@@ -223,7 +223,7 @@ app.post('/Favorites/:UserName',passport.authenticate('jwt', { session: false })
 });
 
 //------------------------------------------------------------------------------------------// DELETE Favorite Movie
-app.post('/Favorites/:UserName/delete/:_id',passport.authenticate('jwt', { session: false }), (req, res) => {
+app.put('/Favorites/:UserName/delete/:_id',passport.authenticate('jwt', { session: false }), (req, res) => {
   users.findOneAndRemove({ UserName: req.params.UserName }, {FavoriteMovies:{
     $pull:   [{
       ObjectId: req.params._id
