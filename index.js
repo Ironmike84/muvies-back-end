5f5a39ab624ef0c20c57e9806b6379eb6d237a13
ironmike84 <michaelgross.tn@gmail.com>
2022-05-01 14:39:14 -0400

    updated

--- index.js
+++ index.js
@@ -225,9 +225,9 @@ app.post('/Favorites/:UserName',passport.authenticate('jwt', { session: false })
 //------------------------------------------------------------------------------------------// DELETE Favorite Movie
 app.post('/Favorites/:UserName/delete/:_id',passport.authenticate('jwt', { session: false }), (req, res) => {
   users.findOneAndUpdate({ UserName: req.params.UserName }, {
-    $unset: { FavoriteMovies: [{
+    $unset:   [{
       ObjectId: req.params._id
-    }] }
+    }] 
                                                       
 },
    { new: true }, // This line makes sure that the updated document is returned
