1bfa80afbc7177e35dd8c1e798b06203c1b7cf8e
ironmike84 <michaelgross.tn@gmail.com>
2022-05-04 18:08:13 -0400

    Drakossssssssssssssssssssssssssss

--- index.js
+++ index.js
@@ -267,6 +267,7 @@ app.put('/Favorites/:UserName/delete/:_id',passport.authenticate('jwt', { sessio
     }
                        
 }),
+{new:false},
     // This line makes sure that the updated document is returned
   (err, updatedUser) => {
     if (err) {
