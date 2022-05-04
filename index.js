903c358c4fa50c240a74dcd522b16f3886d2ecef
ironmike84 <michaelgross.tn@gmail.com>
2022-05-04 18:10:44 -0400

    Drakossssssssssssssssssssssssssssss

--- index.js
+++ index.js
@@ -263,11 +263,11 @@ app.put('/Favorites/:UserName/delete/:_id',passport.authenticate('jwt', { sessio
     },
     {
       "$pull": { 
-        "FavoriteMovies": { ObjectId:req.params._id}
+        "FavoriteMovies": { ObjectId:`${req.params._id}` }
     }
                        
 },
-   { multi: true }, // This line makes sure that the updated document is returned
+   { upsert: true }, // This line makes sure that the updated document is returned
   (err, updatedUser) => {
     if (err) {
       console.error(err);
